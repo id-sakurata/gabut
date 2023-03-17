03ab43a9cd8c856a7386addc0856a2b91df3c98a
riandkbayui <riandkbayui@gmail.com>
2023-03-17 13:54:11 +0700

    remove whatsapp totally

--- index.js
+++ index.js
@@ -4,24 +4,6 @@ const express = require("express");
 const { Client } = require('whatsapp-web.js');
 
 const client = new Client();
-
-client.on('qr', (qr) => {
-    // Generate and scan this code with your phone
-    console.log('QR RECEIVED', qr);
-});
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-client.on('message', msg => {
-    if (msg.body == '!ping') {
-        msg.reply('pong');
-    }
-});
-
-client.initialize()
-
 // Initialize Express
 const app = express();
 // Create GET request
